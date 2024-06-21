31ab704f32fe905e6c4b243ac9ddbf7a52352383
˘Nees <vekker15@gmail.com>
2024-06-21 02:44:09 +0200

    ദ്ദി ༎ຶ‿༎ຶ )  ლ(ಥ益ಥლ)  ദ്ദി ༎ຶ‿༎ຶ )

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { ProfileComponent } from './pages/profile/profile.component';
+import { ProfileComponent } from "./pages/profile/profile.component";
 import { LandingComponent } from './pages/landingpage/landing-page.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 export const routes: Routes = [
     {path: 'coach/:coachID', component: ProfileComponent },
     {path: '', component: LandingComponent },
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent}
 ];
